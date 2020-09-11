f5ccbc7cb84df725d24a4b6517c1997477e2b842
Brandon Green <713150@aacps.org>
2020-09-11 22:34:43 +0000

    I added some ellipse and rectangles. Then finished the tank.

--- index.java
+++ index.java
@@ -14,9 +14,14 @@ void draw() {
   //this rectangle is the start of your army tank
   //all of your programming should be after this 👍 
   rect(200, 200, 100, 50);
-  
-  
- 
+  rect(300, 185 , 100, 15);
+  rect(220, 270, 150, 80);
+  ellipseMode(CORNER);
+  ellipse(145,300, 35, 35);
+  ellipse(175,300, 35, 35);
+  ellipse(205,300, 35, 35);
+ ellipse(235,300, 35, 35);
+ellipse(265,300, 35, 35);
 
   
  
